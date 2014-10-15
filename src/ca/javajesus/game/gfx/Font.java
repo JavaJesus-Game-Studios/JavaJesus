b194bd089f18b92040529adf58d22a770c3b02a4
wookiederk <derek.jow@gmail.com>
2014-10-14 20:08:54 -0700

    For Northway to Read
    You are syncing to the bin folder. Sync to the src folder.

--- src/ca/javajesus/game/gfx/Font.java
+++ src/ca/javajesus/game/gfx/Font.java
@@ -1,20 +1,22 @@
 package ca.javajesus.game.gfx;
 
-public class Font 
-{
-	private static String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ      " + "abcdefghijklmnopqrstuvwxyz      " + "0123456789.,:;'\"!?$%()-=+/      ";
-	
-	public static void render(String msg, Screen screen, int x, int y, int colour, int scale)
-	{
-		msg = msg.toString(); 
-		
-		for (int i = 0; i < msg.length(); i++)
-		{
-			int charIndex = chars.indexOf(msg.charAt(i));
-			if (charIndex >= 0) screen.render(x + (i * 8), y, charIndex + 29 * 32, colour, 0x00, scale);
-			
-		}
-				
-	}
+public class Font {
+    private static String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ      "
+            + "abcdefghijklmnopqrstuvwxyz      "
+            + "0123456789.,:;'\"!?$%()-=+/      ";
+
+    public static void render(String msg, Screen screen, int x, int y,
+            int colour, int scale) {
+        msg = msg.toString();
+
+        for (int i = 0; i < msg.length(); i++) {
+            int charIndex = chars.indexOf(msg.charAt(i));
+            if (charIndex >= 0)
+                screen.render(x + (i * 8), y, charIndex + 29 * 32, colour,
+                        0x00, scale, SpriteSheet.letters);
+
+        }
+
+    }
 
 }
