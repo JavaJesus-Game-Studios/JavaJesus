c83169047dc3dd427730d7f38dfead8e88067a6d
DJow <derek.jow@gmail.com>
2014-10-27 19:17:03 -0700

    NPC Movement -- Its awesome!
    Added different types of NPC movement
    -- Circular Motion movement is bugged

--- src/ca/javajesus/game/entities/NPC.java
+++ src/ca/javajesus/game/entities/NPC.java
@@ -1,6 +1,7 @@
 package ca.javajesus.game.entities;
 
 import java.awt.geom.Ellipse2D;
+import java.util.Random;
 
 import ca.javajesus.game.gfx.Colors;
 import ca.javajesus.game.gfx.Screen;
@@ -9,17 +10,19 @@ import ca.javajesus.level.Level;
 
 public class NPC extends Mob {
 
-	public static NPC npc1 = new NPC(Level.level1, "Knight", 50, 50, 1, 16, 16,
+	public static NPC npc1 = new NPC(Level.level1, "Knight", 200, 100, 1, 16, 16,
 			100, Colors.get(-1, 111, Colors.toHex("#7e7e7e"),
-					Colors.toHex("#FFFFFF")), 0, 2, "square");
-	public static NPC npc2 = new NPC(Level.level1, "Policeman", 50, 100, 1, 16, 16,
-			100, Colors.get(-1, Colors.toHex("#2a2a2a"), Colors.toHex("#000046"), 543), 0, 4, "square");
-	public static NPC npc3 = new NPC(Level.level1, "Jesus", 50, 150, 1, 16, 16,
-			100, Colors.get(-1, 111, 555,Colors.toHex("#ffd89b")), 0, 6, "square");
-	public static NPC npc4 = new NPC(Level.level1, "Citizen-Female", 50, 200, 1, 16, 16,
-			100, Colors.get(-1, 111, 300, 543), 0, 8, "square");
-	public static NPC npc5 = new NPC(Level.level1, "Citizen-Male", 50, 250, 1, 16, 16,
-			100, Colors.get(-1, 111, 300, 543), 0, 0, "square");
+					Colors.toHex("#FFFFFF")), 0, 2, "linear", 20);
+	public static NPC npc2 = new NPC(Level.level1, "Policeman", 200, 200, 1, 16,
+			16, 100, Colors.get(-1, Colors.toHex("#2a2a2a"),
+					Colors.toHex("#000046"), 543), 0, 4, "triangle", 20);
+	public static NPC npc3 = new NPC(Level.level1, "Jesus", 200, 300, 1, 16, 16,
+			100, Colors.get(-1, 111, 555, Colors.toHex("#ffd89b")), 0, 6,
+			"square", 30);
+	public static NPC npc4 = new NPC(Level.level1, "Citizen-Female", 200, 400,
+			1, 16, 16, 100, Colors.get(-1, 111, 300, 543), 0, 8, "cross", 30);
+	public static NPC npc5 = new NPC(Level.level1, "Citizen-Male", 200, 500, 1,
+			16, 16, 100, Colors.get(-1, 111, 300, 543), 0, 0, "circle", 20);
 
 	private double scaledSpeed = 0.35;
 	/** Range that the NPC can walk */
@@ -28,11 +31,25 @@ public class NPC extends Mob {
 	protected int color;
 	protected int xTile;
 	protected int yTile;
+
+	/** Movement type and the distance they travel */
 	protected String walkPath;
+	protected int walkDistance;
+
+	/** NPC origin */
+	protected double xPos;
+	protected double yPos;
+
+	/** Determines direction for NPC movement */
+	protected boolean dir1 = true;
+	protected boolean dir2;
+	protected boolean dir3;
+	protected boolean dir4;
+	protected int tickCount;
 
 	public NPC(Level level, String name, double x, double y, int speed,
 			int width, int height, double defaultHealth, int color, int xTile,
-			int yTile, String walkPath) {
+			int yTile, String walkPath, int walkDistance) {
 		super(level, name, x, y, speed, width, height, SpriteSheet.npcs,
 				defaultHealth);
 		this.walkRadius = new Ellipse2D.Double(x - RADIUS / 2, y - RADIUS / 2,
@@ -41,6 +58,9 @@ public class NPC extends Mob {
 		this.xTile = xTile;
 		this.yTile = yTile;
 		this.walkPath = walkPath;
+		this.walkDistance = walkDistance;
+		this.xPos = x;
+		this.yPos = y;
 	}
 
 	public boolean hasCollided(int xa, int ya) {
@@ -74,27 +94,36 @@ public class NPC extends Mob {
 	public void tick() {
 
 		updateHealth();
+		tickCount++;
 
 		if (isMobCollision()) {
 			moveRandomly();
 			return;
 		}
-		int xa = 0;
-		int ya = 0;
 
-		//switch ()
-
-		if (xa != 0 || ya != 0) {
-			this.hitBox.setLocation(this.hitBox.x + xa, this.hitBox.y + ya);
-			if (isMobCollision()) {
-				this.hitBox.setLocation(this.hitBox.x - xa, this.hitBox.y - ya);
-				isMoving = false;
-				return;
-			}
-			move(xa, ya, scaledSpeed);
-			isMoving = true;
-		} else {
-			isMoving = false;
+		switch (walkPath) {
+		case "linear": {
+			moveLinear();
+			break;
+		}
+		case "triangle": {
+			moveTriangle();
+			break;
+		}
+		case "square": {
+			moveSquare();
+			break;
+		}
+		case "cross": {
+			moveCross();
+			break;
+		}
+		case "circle": {
+			moveCircle();
+			break;
+		}
+		default:
+			break;
 		}
 
 	}
@@ -141,6 +170,200 @@ public class NPC extends Mob {
 		screen.render(xOffset + modifier - (modifier * flipBottom), yOffset
 				+ modifier, (xTile + 1) + (yTile + 1) * 32, color, flipBottom,
 				scale, sheet);
+
+	}
+
+	private void moveLinear() {
+		int xa = 0;
+		int ya = 0;
+		if (dir1) {
+			xa++;
+			if (this.x > this.walkDistance + xPos) {
+				dir1 = false;
+				dir2 = true;
+			}
+		} else if (dir2) {
+			xa--;
+			if (this.x < xPos - this.walkDistance) {
+				dir1 = true;
+				dir2 = false;
+			}
+		}
+		if (xa != 0 || ya != 0) {
+			this.hitBox.setLocation(this.hitBox.x + xa, this.hitBox.y + ya);
+			if (isMobCollision()) {
+				this.hitBox.setLocation(this.hitBox.x - xa, this.hitBox.y - ya);
+				isMoving = false;
+				return;
+			}
+			move(xa, ya, scaledSpeed);
+			isMoving = true;
+		} else {
+			isMoving = false;
+		}
+
+	}
+
+	private void moveTriangle() {
+		int xa = 0;
+		int ya = 0;
+		if (dir1) {
+			xa++;
+			if (this.x > this.walkDistance + xPos) {
+				dir1 = false;
+				dir2 = true;
+			}
+		} else if (dir2) {
+			xa--;
+			ya--;
+			if (this.x < xPos) {
+				dir2 = false;
+				dir3 = true;
+			}
+		} else if (dir3) {
+			xa--;
+			ya++;
+			if (this.x < xPos - this.walkDistance) {
+				dir3 = false;
+				dir1 = true;
+			}
+		}
+		if (xa != 0 || ya != 0) {
+			this.hitBox.setLocation(this.hitBox.x + xa, this.hitBox.y + ya);
+			if (isMobCollision()) {
+				this.hitBox.setLocation(this.hitBox.x - xa, this.hitBox.y - ya);
+				isMoving = false;
+				return;
+			}
+			move(xa, ya, scaledSpeed);
+			isMoving = true;
+		} else {
+			isMoving = false;
+		}
+
+	}
+
+	private void moveSquare() {
+		int xa = 0;
+		int ya = 0;
+		if (dir1) {
+			xa++;
+			if (this.x > this.walkDistance + xPos) {
+				dir1 = false;
+				dir2 = true;
+			}
+		} else if (dir2) {
+			ya++;
+			if (this.y > this.walkDistance + yPos) {
+				dir2 = false;
+				dir3 = true;
+			}
+		} else if (dir3) {
+			xa--;
+			if (this.x < xPos - this.walkDistance) {
+				dir3 = false;
+				dir4 = true;
+			}
+		} else if (dir4) {
+			ya--;
+			if (this.y < yPos - this.walkDistance) {
+				dir4 = false;
+				dir1 = true;
+			}
+		}
+		if (xa != 0 || ya != 0) {
+			this.hitBox.setLocation(this.hitBox.x + xa, this.hitBox.y + ya);
+			if (isMobCollision()) {
+				this.hitBox.setLocation(this.hitBox.x - xa, this.hitBox.y - ya);
+				isMoving = false;
+				return;
+			}
+			move(xa, ya, scaledSpeed);
+			isMoving = true;
+		} else {
+			isMoving = false;
+		}
+
+	}
+
+	private void moveCross() {
+		int xa = 0;
+		int ya = 0;
+		if (!dir1 && !dir2 && !dir3 && !dir4) {
+			Random random = new Random();
+			switch (random.nextInt(4)) {
+			case 0: {
+				dir1 = true;
+				break;
+			}
+			case 1: {
+				dir2 = true;
+				break;
+			}
+			case 2: {
+				dir3 = true;
+				break;
+			}
+			case 3: {
+				dir4 = true;
+				break;
+			}
+			}
+		}
+
+		if (dir1) {
+			xa++;
+			if (this.x > this.walkDistance + xPos) {
+				dir1 = false;
+			}
+		} else if (dir2) {
+			ya++;
+			if (this.y > this.walkDistance + yPos) {
+				dir2 = false;
+			}
+		} else if (dir3) {
+			xa--;
+			if (this.x < xPos - this.walkDistance) {
+				dir3 = false;
+			}
+		} else if (dir4) {
+			ya--;
+			if (this.y < yPos - this.walkDistance) {
+				dir4 = false;
+			}
+		}
+		if (xa != 0 || ya != 0) {
+			this.hitBox.setLocation(this.hitBox.x + xa, this.hitBox.y + ya);
+			if (isMobCollision()) {
+				this.hitBox.setLocation(this.hitBox.x - xa, this.hitBox.y - ya);
+				isMoving = false;
+				return;
+			}
+			move(xa, ya, scaledSpeed);
+			isMoving = true;
+		} else {
+			isMoving = false;
+		}
+
+	}
+
+	private void moveCircle() {
+		
+		// This is obviously wrong. Someone can do the proper math to fix it
+		int xa = (int) Math.cos(tickCount);
+		int ya = (int) Math.sin(tickCount);
+		if (xa != 0 || ya != 0) {
+			this.hitBox.setLocation(this.hitBox.x + xa, this.hitBox.y + ya);
+			if (isMobCollision()) {
+				this.hitBox.setLocation(this.hitBox.x - xa, this.hitBox.y - ya);
+				isMoving = false;
+				return;
+			}
+			move(xa, ya, scaledSpeed);
+			isMoving = true;
+		} else {
+			isMoving = false;
+		}
 
 	}
 
