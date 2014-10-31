141c779c15d046512f592accd00b3144be7d2ceb
Shtevay <stephennway@Stephens-MacBook-Pro.local>
2014-10-30 18:40:30 -0700

    Conflict Fix

--- src/ca/javajesus/game/entities/vehicles/CenturyLeSabre.java
+++ src/ca/javajesus/game/entities/vehicles/CenturyLeSabre.java
@@ -9,15 +9,12 @@ import ca.javajesus.level.Level;
 
 public class CenturyLeSabre extends Vehicle {
 
-<<<<<<< Updated upstream
-	protected int color;
+	protected int color1;
 	private Random random = new Random();
 
-=======
 	protected final int color = Colors.get(-1, 111, Colors.toHex("#ff1e00"),
 			Colors.toHex("#92fff6"));
 	
->>>>>>> Stashed changes
 	public CenturyLeSabre(Level level, String name, double x, double y,
 			int speed, int width, int height, SpriteSheet sheet,
 			double defaultHealth) {
@@ -28,25 +25,25 @@ public class CenturyLeSabre extends Vehicle {
 	private void getColor() {
 		switch (random.nextInt(5)) {
 		case 0: {
-			color = Colors.get(-1, 444, 222, 111);
+			color1 = Colors.get(-1, 444, 222, 111);
 			break;
 		}
 		case 1: {
-			color = Colors.get(-1, 444, 222, 111);
+			color1 = Colors.get(-1, 444, 222, 111);
 			break;
 		}
 
 		case 2: {
-			color = Colors.get(-1, 444, 222, 111);
+			color1 = Colors.get(-1, 444, 222, 111);
 			break;
 		}
 		case 3: {
-			color = Colors.get(-1, 444, 222, 111);
+			color1 = Colors.get(-1, 444, 222, 111);
 			break;
 		}
 		default: {
 
-			color = Colors.get(-1, 444, 222, 111);
+			color1 = Colors.get(-1, 444, 222, 111);
 			break;
 		}
 		}
@@ -82,96 +79,96 @@ public class CenturyLeSabre extends Vehicle {
 
 		// Upper body 1
 		screen.render(xOffset + (modifier * flipTop), yOffset, xTile + yTile
-				* 32, color, flipTop, scale, sheet);
+				* 32, color1, flipTop, scale, sheet);
 		// Upper Body 2
 		screen.render(xOffset + modifier - (modifier * flipTop), yOffset,
-				(xTile + 1) + yTile * 32, color, flipTop, scale, sheet);
+				(xTile + 1) + yTile * 32, color1, flipTop, scale, sheet);
 
 		// Upper Body 3
 		screen.render(xOffset + 2 * modifier - (modifier * flipTop), yOffset,
-				(xTile + 2) + yTile * 32, color, flipTop, scale, sheet);
+				(xTile + 2) + yTile * 32, color1, flipTop, scale, sheet);
 
 		// Upper Body 4
 		screen.render(xOffset + 3 * modifier - (modifier * flipTop), yOffset,
-				(xTile + 3) + yTile * 32, color, flipTop, scale, sheet);
+				(xTile + 3) + yTile * 32, color1, flipTop, scale, sheet);
 
 		// Second Body 1
 		screen.render(xOffset + (modifier * flipBottom), yOffset + modifier,
-				xTile + (yTile + 1) * 32, color, flipBottom, scale, sheet);
+				xTile + (yTile + 1) * 32, color1, flipBottom, scale, sheet);
 
 		// Second Body 2
 		screen.render(xOffset + modifier - (modifier * flipBottom), yOffset
-				+ modifier, (xTile + 1) + (yTile + 1) * 32, color, flipBottom,
+				+ modifier, (xTile + 1) + (yTile + 1) * 32, color1, flipBottom,
 				scale, sheet);
 
 		// Second Body 3
 		screen.render(xOffset + 2 * modifier - (modifier * flipBottom), yOffset
-				+ modifier, (xTile + 2) + (yTile + 1) * 32, color, flipBottom,
+				+ modifier, (xTile + 2) + (yTile + 1) * 32, color1, flipBottom,
 				scale, sheet);
 
 		// Second Body 4
 		screen.render(xOffset + 3 * modifier - (modifier * flipBottom), yOffset
-				+ modifier, (xTile + 3) + (yTile + 1) * 32, color, flipBottom,
+				+ modifier, (xTile + 3) + (yTile + 1) * 32, color1, flipBottom,
 				scale, sheet);
 
 		// Third Body 1
 		screen.render(xOffset + (modifier * flipBottom),
-				yOffset + 2 * modifier, xTile + (yTile + 2) * 32, color,
+				yOffset + 2 * modifier, xTile + (yTile + 2) * 32, color1,
 				flipBottom, scale, sheet);
 
 		// Third Body 2
 		screen.render(xOffset + modifier - (modifier * flipBottom), yOffset + 2
-				* modifier, (xTile + 1) + (yTile + 2) * 32, color, flipBottom,
+				* modifier, (xTile + 1) + (yTile + 2) * 32, color1, flipBottom,
 				scale, sheet);
 
 		// Third Body 3
 		screen.render(xOffset + 2 * modifier - (modifier * flipBottom), yOffset
-				+ 2 * modifier, (xTile + 2) + (yTile + 2) * 32, color,
+				+ 2 * modifier, (xTile + 2) + (yTile + 2) * 32, color1,
 				flipBottom, scale, sheet);
 
 		// Third Body 4
 		screen.render(xOffset + 3 * modifier - (modifier * flipBottom), yOffset
-				+ 2 * modifier, (xTile + 3) + (yTile + 2) * 32, color,
+				+ 2 * modifier, (xTile + 3) + (yTile + 2) * 32, color1,
 				flipBottom, scale, sheet);
 
 		// Fourth Body 1
 		screen.render(xOffset + (modifier * flipBottom),
-				yOffset + 3 * modifier, xTile + (yTile + 3) * 32, color,
+				yOffset + 3 * modifier, xTile + (yTile + 3) * 32, color1,
 				flipBottom, scale, sheet);
 
 		// Fourth Body 2
 		screen.render(xOffset + modifier - (modifier * flipBottom), yOffset + 3
-				* modifier, (xTile + 1) + (yTile + 3) * 32, color, flipBottom,
+				* modifier, (xTile + 1) + (yTile + 3) * 32, color1, flipBottom,
 				scale, sheet);
 
 		// Fourth Body 3
 		screen.render(xOffset + 2 * modifier - (modifier * flipBottom), yOffset
-				+ 3 * modifier, (xTile + 2) + (yTile + 3) * 32, color,
+				+ 3 * modifier, (xTile + 2) + (yTile + 3) * 32, color1,
 				flipBottom, scale, sheet);
 
 		// Fourth Body 4
 		screen.render(xOffset + 3 * modifier - (modifier * flipBottom), yOffset
-				+ 3 * modifier, (xTile + 3) + (yTile + 3) * 32, color,
+				+ 3 * modifier, (xTile + 3) + (yTile + 3) * 32, color1,
 				flipBottom, scale, sheet);
 
 		// Lower Body 1
 		screen.render(xOffset + (modifier * flipBottom),
-				yOffset + 4 * modifier, xTile + (yTile + 4) * 32, color,
+				yOffset + 4 * modifier, xTile + (yTile + 4) * 32, color1,
 				flipBottom, scale, sheet);
 
 		// Lower Body 2
 		screen.render(xOffset + modifier - (modifier * flipBottom), yOffset + 4
-				* modifier, (xTile + 1) + (yTile + 4) * 32, color, flipBottom,
+				* modifier, (xTile + 1) + (yTile + 4) * 32, color1, flipBottom,
 				scale, sheet);
 
 		// Lower Body 3
 		screen.render(xOffset + 2 * modifier - (modifier * flipBottom), yOffset
-				+ 4 * modifier, (xTile + 2) + (yTile + 4) * 32, color,
+				+ 4 * modifier, (xTile + 2) + (yTile + 4) * 32, color1,
 				flipBottom, scale, sheet);
 
 		// Lower Body 4
 		screen.render(xOffset + 3 * modifier - (modifier * flipBottom), yOffset
-				+ 4 * modifier, (xTile + 3) + (yTile + 4) * 32, color,
+				+ 4 * modifier, (xTile + 3) + (yTile + 4) * 32, color1,
 				flipBottom, scale, sheet);
 
 	}
