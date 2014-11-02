d1750b9f06ca9708d5f1132ff8f66870983dcca8
DJow <derek.jow@gmail.com>
2014-11-02 11:42:41 -0800

    Conflict Fix

--- src/ca/javajesus/level/tile/TreeTile.java
+++ src/ca/javajesus/level/tile/TreeTile.java
@@ -8,17 +8,18 @@ public class TreeTile extends MultiTile {
 
 	private static Random random = new Random();
 
-	/* Stephen, fix all the colors */
-	public static Tile LEAF1 = new BaseTile(10, 0, 7, getColor(), 1);
-	public static Tile LEAF2 = new BaseTile(11, 1, 7, getColor(), 2);
-	public static Tile LOG1 = new BaseTile(12, 0, 8, Colors.get(131, 000,
-			Colors.toHex("#BFAD47"), 1), 3);
-	public static Tile LOG2 = new BaseTile(13, 1, 8, Colors.get(131, 000,
-			Colors.toHex("#BFAD47"), 1), 4);
-	public static Tile LOG3 = new BaseTile(14, 0, 9, Colors.get(131, 000,
-			Colors.toHex("#BFAD47"), 1), 5);
-	public static Tile LOG4 = new BaseTile(15, 1, 9, Colors.get(131, 000,
-			Colors.toHex("#BFAD47"), 1), 6);
+	public static final Tile LEAF1 = new BaseTile(10, 0, 7, Colors.get(131, 111,
+			-1, Colors.toHex("#008015")), 1);
+	public static final Tile LEAF2 = new BaseTile(11, 1, 7, Colors.get(131, 111,
+			-1 , Colors.toHex("#008015")), 2);
+	public static final Tile LOG1 = new BaseTile(12, 0, 8, Colors.get(131, 111,
+			Colors.toHex("#624300"), 141), 3);
+	public static final Tile LOG2 = new BaseTile(13, 1, 8, Colors.get(131, 111,
+			Colors.toHex("#624300"), 141), 4);
+	public static final Tile LOG3 = new BaseTile(14, 0, 9, Colors.get(131, 111,
+			Colors.toHex("#624300"), 141), 5);
+	public static final Tile LOG4 = new BaseTile(15, 1, 9, Colors.get(131, 111,
+			Colors.toHex("#624300"), 141), 6);
 	
 	protected static Tile[] blockList = { LEAF1, LEAF2, LOG1, LOG2, LOG3, LOG4 };
 
@@ -26,6 +27,7 @@ public class TreeTile extends MultiTile {
 		super(id, levelColour, width, height, blockList);
 	}
 
+	// Maybe use this later
 	private static int getColor() {
 		switch (random.nextInt(5)) {
 		case 0:
