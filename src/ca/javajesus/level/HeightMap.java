59604960779b09508d51bf28db331f243307fcff
stp0023 <stp0023@gmail.com>
2015-01-20 09:53:59 -0800

    heightmap 2
    Apparently I didn't save before syncing, here is the full code for
    heightmap. I also tried adding in comments to make methods easier to
    use.

--- src/ca/javajesus/level/HeightMap.java
+++ src/ca/javajesus/level/HeightMap.java
@@ -1,6 +1,140 @@
 package ca.javajesus.level;
 
+import java.util.Random;
+
 public class HeightMap
 {
+    public int height;
+    public int width;
 
+    /**
+     * Methods for generating and using a heightmap for random worlds
+     * 
+     * @param height
+     *            :The desired height of the heightmap
+     * 
+     * @param width
+     *            :The desired width of the heightmap
+     */
+    public HeightMap(int height, int width)
+    {
+        this.height = height;
+        this.width = width;
+    }
+
+    /**
+     * Creates a heightmap of height x width
+     * 
+     * @param cycles
+     *            :the number of times the heightmap will be cycled. 4 cycles is
+     *            recommended.
+     * 
+     */
+    public int[][] generateHeightMap(int cycles)
+    {
+        Random rand = new Random();
+        int[][] heightmap = new int[height][width];
+
+        for (int row = 0; row < heightmap.length; row++)
+        {
+            for (int col = 0; col < heightmap[0].length; col++)
+            {
+                heightmap[row][col] = rand.nextInt(1000);
+            }
+        }
+
+        for (int cycle = 0; cycle < cycles; cycle++)
+        {
+            for (int row = 0; row < heightmap.length; row++)
+            {
+                for (int col = 0; col < heightmap[0].length; col++)
+                {
+                    if (row == 0)
+                    {
+                        if (col == 0)
+                        {
+                            heightmap[row][col] = (heightmap[row][col + 1]
+                                    + heightmap[row + 1][col + 1] + heightmap[row + 1][col]) / 3;
+                        } else if (col == heightmap[0].length - 1)
+                        {
+                            heightmap[row][col] = (heightmap[row][col - 1]
+                                    + heightmap[row + 1][col - 1] + heightmap[row + 1][col]) / 3;
+                        } else
+                        {
+                            heightmap[row][col] = (heightmap[row][col - 1]
+                                    + heightmap[row + 1][col - 1]
+                                    + heightmap[row + 1][col]
+                                    + heightmap[row + 1][col + 1] + heightmap[row][col + 1]) / 5;
+                        }
+                    }
+                    if (row == heightmap.length - 1)
+                    {
+                        if (col == 0)
+                        {
+                            heightmap[row][col] = (heightmap[row][col + 1]
+                                    + heightmap[row - 1][col + 1] + heightmap[row - 1][col]) / 3;
+                        } else if (col == heightmap[0].length - 1)
+                        {
+                            heightmap[row][col] = (heightmap[row][col - 1]
+                                    + heightmap[row - 1][col - 1] + heightmap[row - 1][col]) / 3;
+                        } else
+                        {
+                            heightmap[row][col] = (heightmap[row][col - 1]
+                                    + heightmap[row - 1][col - 1]
+                                    + heightmap[row - 1][col]
+                                    + heightmap[row - 1][col + 1] + heightmap[row][col + 1]) / 5;
+                        }
+                    }
+                    if (row > 0 && row < heightmap.length)
+                    {
+                        if (col == 0)
+                        {
+                            heightmap[row][col] = (heightmap[row - 1][col]
+                                    + heightmap[row - 1][col + 1]
+                                    + heightmap[row][col + 1]
+                                    + heightmap[row + 1][col + 1] + heightmap[row + 1][col]) / 5;
+                        } else if (col == heightmap[0].length)
+                        {
+                            heightmap[row][col] = (heightmap[row - 1][col]
+                                    + heightmap[row - 1][col - 1]
+                                    + heightmap[row][col - 1]
+                                    + heightmap[row + 1][col - 1] + heightmap[row - 1][col]) / 5;
+                        } else
+                        {
+                            heightmap[row][col] = (heightmap[row - 1][col - 1]
+                                    + heightmap[row - 1][col]
+                                    + heightmap[row - 1][col + 1]
+                                    + heightmap[row][col + 1]
+                                    + heightmap[row + 1][col + 1]
+                                    + heightmap[row + 1][col]
+                                    + heightmap[row + 1][col - 1] + heightmap[row][col - 1]) / 8;
+                        }
+                    }
+                }
+            }
+        }
+        return heightmap;
+    }
+
+    /**
+     * Finds the average of the entire heightmap, necessary to find if water,
+     * ground, or mountain tile
+     *
+     * @param heightmap
+     *            : the heightmap you want an average of
+     * 
+     */
+    public int getAverage(int[][] heightmap)
+    {
+        int sumTotal = 0;
+        for (int row = 0; row < heightmap.length; row++)
+        {
+            for (int col = 0; col < heightmap[0].length; col++)
+            {
+                sumTotal += heightmap[row][col];
+            }
+        }
+        int average = sumTotal / (heightmap.length * heightmap[0].length);
+        return average;
+    }
 }
