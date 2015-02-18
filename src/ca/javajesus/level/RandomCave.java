31f5a587b2a13281caa1d1673a59a5199c2be29b
stp0023 <stp0023@gmail.com>
2015-02-18 11:02:13 -0800

    cave fix

--- src/ca/javajesus/level/RandomCave.java
+++ src/ca/javajesus/level/RandomCave.java
@@ -9,12 +9,11 @@ public class RandomCave extends Level {
 
 	public RandomCave(int height, int width, int cycles) {
 		super(width, height);
-		caveGen = new CaveGeneration(height, width, 5);
-		caveMap = caveGen.generateCave();
-		spawnPoint = caveGen.getSpawnPoint();
+		
 	}
 	
 	protected void generateLevel() {
+		caveMap = new CaveGeneration(height, width, 5).generateCave();
 		for (int row = 0; row < height; row++) {
 			for (int col = 0; col < width; col++) {
 				int tile = col + row * width;
