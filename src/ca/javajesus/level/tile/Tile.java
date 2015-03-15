08a7f7273ffe872aa654bed9a44e18432df4bec2
Shtevay <stephennway@Stephens-MacBook-Pro.local>
2015-03-15 14:39:50 -0700

    Progress on Interiors
    They look nicer now

--- src/ca/javajesus/level/tile/Tile.java
+++ src/ca/javajesus/level/tile/Tile.java
@@ -40,8 +40,6 @@ public abstract class Tile {
 			Colors.get(-1, Colors.fromHex("#DBEDD2"), 115, -1), 0xFF64FFFF, 1000);
 	public static final Tile ROAD3 = new BaseTile(9, 2, 2, Colors.get(-1, 000,
 			Colors.fromHex("#BFAD47"), -1), 0xFFFFE400);
-	public static final Tile WOOD_WALL = new BasicSolidTile(10, 2, 0, Colors.get(-1,
-			Colors.fromHex("#693609"), 000, -1), 0xFF6d4300);
 	public static final Tile MUD = new BaseTile(11, 0, 2, Colors.get(-1, Colors.fromHex("#4a2d00"),
 			-1, -1), 0xFF372201);
 	public static final Tile FARMPLOT1 = new BaseTile(12, 2, 1, Colors.get(-1, Colors.fromHex("#0c6600"),
@@ -63,7 +61,7 @@ public abstract class Tile {
 	public static final Tile CAVEFLOOR = new BaseTile(20, 0, 2, Colors.get(-1, Colors.fromHex("#AEAEAD"),-1, -1),
 			0xFFc3beb7);
 	public static final Tile WOODFLOOR = new BaseTile(21, 6, 0, Colors.get(-1,
-			Colors.fromHex("#6d4300"), 000, -1), 0xFF944500);
+			Colors.fromHex("#6d4300"), Colors.fromHex("#3e1900"), -1), 0xFF944500);
 	public static final Tile METALWALL = new BasicSolidTile(22, 5, 0, Colors.get(-1, Colors.fromHex("#a6a6a6"),
 			Colors.fromHex("#939393"), Colors.fromHex("#4b3618")), 0xFFb6b6b6);
 	public static final Tile RIGHTCARPET = new BaseTile(23, 7, 1, Colors.get(-1,
@@ -74,6 +72,23 @@ public abstract class Tile {
 			Colors.fromHex("#a40101"), Colors.fromHex("#ebcb00"), -1), 0xFFff00ff);
 	public static final Tile BOTTOMCARPET = new BaseTile(27, 8, 1, Colors.get(-1,
 			Colors.fromHex("#a40101"), Colors.fromHex("#ebcb00"), -1), 0xFF6600a7);
+	public static final Tile WOOD_WALL_HORIZONTAL_UP = new BasicSolidTile(10, 7, 0, Colors.get(-1,
+			Colors.fromHex("#693609"), Colors.fromHex("#7c3f0f"), Colors.fromHex("#48260b")), 0xFF6d4300);
+	public static final Tile WOOD_WALL_HORIZONTAL_DOWN = new BasicSolidTile(28, 8, 0, Colors.get(-1,
+			Colors.fromHex("#693609"), Colors.fromHex("#7c3f0f"), Colors.fromHex("#48260b")), 0xFF835100);
+	public static final Tile WOOD_WALL_VERTICAL_LEFT = new BasicSolidTile(29, 9, 0, Colors.get(-1,
+			Colors.fromHex("#693609"), Colors.fromHex("#7c3f0f"), Colors.fromHex("#48260b")), 0xFF5a3801);
+	public static final Tile WOOD_WALL_VERTICAL_RIGHT = new BasicSolidTile(30, 10, 0, Colors.get(-1,
+			Colors.fromHex("#693609"), Colors.fromHex("#7c3f0f"), Colors.fromHex("#48260b")), 0xFF472c00);
+	public static final Tile WOOD_WALL_CORNER_RIGHT_UP = new BasicSolidTile(31, 11, 0, Colors.get(-1,
+			Colors.fromHex("#693609"), Colors.fromHex("#7c3f0f"), Colors.fromHex("#48260b")), 0xFF520101);
+	public static final Tile WOOD_WALL_CORNER_LEFT_UP = new BasicSolidTile(32, 12, 0, Colors.get(-1,
+			Colors.fromHex("#693609"), Colors.fromHex("#7c3f0f"), Colors.fromHex("#48260b")), 0xFF420101);
+	public static final Tile WOOD_WALL_CORNER_RIGHT_DOWN = new BasicSolidTile(33, 14, 0, Colors.get(-1,
+			Colors.fromHex("#693609"), Colors.fromHex("#7c3f0f"), Colors.fromHex("#48260b")), 0xFF7a0000);
+	public static final Tile WOOD_WALL_CORNER_LEFT_DOWN = new BasicSolidTile(34, 13, 0, Colors.get(-1,
+			Colors.fromHex("#693609"), Colors.fromHex("#7c3f0f"), Colors.fromHex("#48260b")), 0xFF310000);
+
 
 
 	protected byte id;
