f20bdd4b105406b23ed25d567a7b2424e86418d8
Siderskini <siddharth.viswanathan@gmail.com>
2015-04-07 15:04:52 -0700

    Fixed Spelling
    Sequoia is an unexpectedly hard word to spell correctly.

--- src/ca/javajesus/game/entities/structures/trees/Tree.java
+++ src/ca/javajesus/game/entities/structures/trees/Tree.java
@@ -13,7 +13,7 @@ public class Tree extends SolidEntity {
 	public static Sprite DEAD_SEQUOIA = new Sprite("/Buildings/Trees/Coniferous_Trees/Dead_Sequoia.png");
 	public static Sprite SMALL_SEQUOIA = new Sprite("/Buildings/Trees/Coniferous_Trees/Sequoia_Small.png");
 	public static Sprite MEDIUM_SEQUOIA = new Sprite("/Buildings/Trees/Coniferous_Trees/Sequoia_Medium.png");
-	public static Sprite LARGE_SEQUOIA = new Sprite("/Buildings/Trees/Coniferous_Trees/Sequioa_Large.png");
+	public static Sprite LARGE_SEQUOIA = new Sprite("/Buildings/Trees/Coniferous_Trees/Sequoia_Large.png");
 	public static Sprite SMALL_DECIDUOUS = new Sprite("/Buildings/Trees/Deciduous_Trees/Small_Tree.png");
 	
 	private Sprite sprite;
