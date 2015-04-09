86b0b99497550b29a13fececf014f95ef7d9b1b0
stp0023 <stp0023@gmail.com>
2015-04-09 15:30:45 -0700

    stuff

---
+++ src/ca/javajesus/saves/SerialWrite.java
@@ -0,0 +1,25 @@
+package ca.javajesus.saves;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class SerialWrite {
+	private FileOutputStream fileOut;
+	private ObjectOutputStream objectOut;
+	private String path;
+	
+	public SerialWrite() throws IOException {
+		if (System.getProperty("os.name").indexOf("Windows") >= 0) {
+			path = "C:/Users/" + System.getProperty("user.name") + "/Documents/JavaJesus/save.ser";
+		} else {
+			path = "Your filepath is in another castle";
+		}
+		fileOut = new FileOutputStream(path);
+		objectOut = new ObjectOutputStream(fileOut);
+	}
+	
+	private void objectWrite(Object e) throws IOException {
+		objectOut.writeObject(e);
+	}
+}
