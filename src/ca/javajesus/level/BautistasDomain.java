d9424205d7d55c0ef95cc9b196f16eec1651bfc3
Shtevay <stephennway@Stephens-MacBook-Pro.local>
2015-04-10 13:13:09 -0700

    Beginnins of San Cisco, and Domain of Bautista for Jeff to start

---
+++ src/ca/javajesus/level/BautistasDomain.java
@@ -0,0 +1,49 @@
+package ca.javajesus.level;
+
+import java.awt.Point;
+
+import ca.javajesus.game.entities.structures.Castle;
+import ca.javajesus.game.entities.structures.CastleTower;
+import ca.javajesus.game.entities.structures.CatholicChapel;
+import ca.javajesus.game.entities.structures.CaveEntrance;
+import ca.javajesus.game.entities.structures.GenericHospital;
+import ca.javajesus.game.entities.structures.GunStore;
+import ca.javajesus.game.entities.structures.Hotel;
+import ca.javajesus.game.entities.structures.Hut;
+import ca.javajesus.game.entities.structures.NiceHouse;
+import ca.javajesus.game.entities.structures.NiceHouse2;
+
+public class BautistasDomain extends Level{
+	public BautistasDomain(){
+		super("/Levels/Cities/Domain_of_Rancher_Bautista", true);
+		this.spawnPoint = new Point(2896, 64);
+		startingSpawnPoint = new Point(2896, 64);
+
+	}
+
+	protected void initNPCPlacement() {
+		
+	}
+
+	 
+	protected void initSpawnerPlacement() {
+		 
+		
+	}
+
+	 
+	protected void initChestPlacement() {
+		 
+		
+	}
+
+	 
+	protected void otherEntityPlacement() {
+		
+		
+		//Sequoa City
+		//Bautista's Cattle Ranch
+		
+		
+	}
+}
