99fb0f07dfd42c8ff94cc60ac267b30a6bfcf798
JDalli <dalli2015jeffrey@gmail.com>
2015-04-17 10:50:12 -0700

    Map Placement - Edge of the Woods (Top)

---
+++ src/ca/javajesus/level/EdgeOfTheWoodsTop.java
@@ -0,0 +1,45 @@
+package ca.javajesus.level;
+
+import java.awt.Point;
+
+import ca.javajesus.game.entities.structures.CaveEntrance;
+import ca.javajesus.game.entities.structures.Hotel;
+import ca.javajesus.game.entities.structures.NiceHouse;
+import ca.javajesus.game.entities.structures.PoorHouse;
+
+public class EdgeOfTheWoodsTop extends Level
+{
+
+    public EdgeOfTheWoodsTop()
+    {
+        super("/Levels/Wilderness_Areas/Edge_of_the_Woods_Top.png", true);
+        this.spawnPoint = new Point(1832, 1544);
+        startingSpawnPoint = new Point(1832, 1544);
+    }
+
+    protected void initNPCPlacement() {
+        
+    }
+
+    protected void initSpawnerPlacement() {
+        
+    }
+
+    protected void initChestPlacement() {
+        
+    }
+
+    protected void otherEntityPlacement() {
+        this.addEntity(new CaveEntrance(this, 1856, 688));
+        this.addEntity(new CaveEntrance(this, 2616, 1176));
+        this.addEntity(new Hotel(this, 1872, 1408));
+        this.addEntity(new PoorHouse(this, 2040, 1536));
+        this.addEntity(new PoorHouse(this, 2056, 1632));
+        this.addEntity(new PoorHouse(this, 1920, 1568));
+        this.addEntity(new PoorHouse(this, 1936, 1672));
+        this.addEntity(new NiceHouse(this, 2048, 1416));
+        this.addEntity(new CaveEntrance(this, 2448, 2440));
+
+    }
+
+}
