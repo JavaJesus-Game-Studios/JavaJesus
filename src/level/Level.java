6dfa7d317afa86fc09a47e2fb5d3b1dd45937b7f
Derek <derek.jow@gmail.com>
2016-05-07 01:06:08 -0700

    Part 8.1
    Started work on the level class

--- src/level/Level.java
+++ src/level/Level.java
@@ -1,9 +1,21 @@
 package level;
 
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.imageio.ImageIO;
+import javax.sound.sampled.Clip;
+
 import game.Game;
 import game.SoundHandler;
 import game.entities.Entity;
-import game.entities.FireEntity;
 import game.entities.Mob;
 import game.entities.Player;
 import game.entities.Spawner;
@@ -12,108 +24,101 @@ import game.entities.projectiles.Projectile;
 import game.entities.vehicles.Vehicle;
 import game.graphics.JJFont;
 import game.graphics.Screen;
-
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-
-import javax.imageio.ImageIO;
-import javax.sound.sampled.Clip;
-
-import save.SaveFile;
 import level.tile.Tile;
+import save.SaveFile;
 
+/*
+ * A Level contains a set of tiles that can be displayed on the screen
+ * It also holds a set of all entities that interact with each other
+ */
 public abstract class Level implements Serializable {
 
 	private static final long serialVersionUID = -5963535226522522466L;
 
-	protected int[] tiles;
-	public int width;
-	public int height;
-	protected List<Entity> entities = new CopyOnWriteArrayList<Entity>();
-	protected List<Mob> mobs = new CopyOnWriteArrayList<Mob>();
-	protected List<Player> players = new CopyOnWriteArrayList<Player>();
-	public List<Mob> killList = new CopyOnWriteArrayList<Mob>();
+	// all tiles on each level
+	private int[] tiles;
+
+	// width and height in terms of tiles
+	private int width, height;
+
+	// instance of the player on the level
+	private Player player;
+
+	// list of all entities on the map
+	private List<Entity> entities = new ArrayList<Entity>(Game.ENTITY_LIMIT);
+
+	// list of all mobs on the map
+	private List<Mob> mobs = new ArrayList<Mob>(Game.ENTITY_LIMIT);
+
+	// image path to load a level
 	private String imagePath;
-	private transient BufferedImage image;
-	public Point spawnPoint;
-	public Point startingSpawnPoint;
-	
-	public SaveFile saveData;
-	
-	public boolean initEntities = false;
 
-	protected int[] tileColours;
+	// where the player will go when changing levels here
+	private Point spawnPoint;
 
-	public boolean isLoaded = false;
-	private int loadType = 0;
+	// where the level data is saved
+	private SaveFile saveData;
+
+	// whether or not entities have been added on the map
+	private boolean initEntities;
+
+	// whether or not the map is loaded
+	private boolean isLoaded;
+
+	// name of the level
 	private String name;
-	
-	private Screen screen;
 
-	public static Rectangle screenRectangle = new Rectangle(500, 500);
+	// the range of how many entities to render/tick on the screen
+	public static final Rectangle renderRange = new Rectangle(500, 500);
 
-	public Level(String imagePath, boolean loadNow, String name, Screen screen) {
+	/**
+	 * Creates a level from the specified image path
+	 * 
+	 * @param imagePath
+	 *            the image path
+	 * @param loadNow
+	 *            whether or not to load it now
+	 * @param name
+	 *            the name of thsi levvel
+	 */
+	public Level(String imagePath, boolean loadNow, String name) {
 		this.name = name;
 		this.saveData = new SaveFile(name);
 		spawnPoint = new Point(0, 0);
-		startingSpawnPoint = new Point(0, 0);
 		if (imagePath != null) {
 			this.imagePath = imagePath;
-			loadType = 0;
 			if (loadNow) {
 				isLoaded = true;
-				this.loadLevelFromFile();
+				loadLevelFromFile();
 			}
 		} else {
-			this.width = 64;
-			this.height = 64;
-			tiles = new int[width * height];
-			loadType = 1;
-			if (loadNow) {
-				isLoaded = true;
-				this.generateLevel();
-			}
-		}
-		this.screen = screen;
-	}
-
-	public void load() {
-		if (!isLoaded) {
-			if (loadType == 0) {
-				this.loadLevelFromFile();
-			} else if (loadType == 1) {
-				this.generateLevel();
-			} else {
-				generateLevel();
-			}
-			init();
-			isLoaded = true;
+			throw new RuntimeException("Level path is null");
 		}
 	}
-	
-	public Clip getBackgroundMusic() {
 
-		return SoundHandler.sound.background1;
-	}
-
-	public Level(int width, int height, boolean loadNow, Screen screen) {
+	/**
+	 * Creates a randomly generated level
+	 * 
+	 * @param width
+	 *            the tiles in the width
+	 * @param height
+	 *            the tiles in the height
+	 * @param loadNow
+	 *            whether or not to load it now
+	 * @param name
+	 *            the name of this level
+	 */
+	public Level(int width, int height, boolean loadNow, String name) {
 		spawnPoint = new Point(0, 0);
-		startingSpawnPoint = new Point(0, 0);
 		this.width = width;
 		this.height = height;
+		this.name = name;
+		this.saveData = new SaveFile(name);
 		tiles = new int[width * height];
-		loadType = 2;
 		if (loadNow) {
 			isLoaded = true;
 			generateLevel();
 		}
-		this.screen = screen;
 	}
 
 	protected abstract void initNPCPlacement();
@@ -126,40 +131,73 @@ public abstract class Level implements Serializable {
 
 	protected abstract void initMapTransporters();
 
+	/**
+	 * Loads a level
+	 */
+	public void load() {
+		if (!isLoaded) {
+			if (imagePath != null) {
+				loadLevelFromFile();
+			} else {
+				generateLevel();
+			}
+			loadEntities();
+			isLoaded = true;
+		}
+	}
+
+	/**
+	 * TODO create an instance variable
+	 * 
+	 * @return the background music for this level
+	 */
+	public Clip getBackgroundMusic() {
+
+		return SoundHandler.background1;
+	}
+
+	/**
+	 * Loads an image from the file
+	 */
 	private void loadLevelFromFile() {
 		try {
-			this.image = ImageIO.read(Level.class.getResource(this.imagePath));
-			this.width = this.image.getWidth();
-			this.height = this.image.getHeight();
+			// load the file
+			BufferedImage image = ImageIO.read(Level.class.getResource(imagePath));
+			width = image.getWidth();
+			height = image.getHeight();
 			tiles = new int[width * height];
-			tileColours = this.image
-					.getRGB(0, 0, width, height, null, 0, width);
-			this.loadTiles();
+
+			// get the tile colors
+			int[] tileColors = image.getRGB(0, 0, width, height, null, 0, width);
+
+			// initialize the tiles
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++) {
+					tileCheck: for (Tile t : Tile.tiles) {
+						if (t != null && t.getLevelColor() == tileColors[x + y * width]) {
+							if (t == Tile.GRASS) {
+								t = Tile.GRASS();
+							} else if (t == Tile.WASTELAND_GROUND1) {
+								t = Tile.CONCRETE();
+							}
+							this.tiles[x + y * width] = t.getId();
+							break tileCheck;
+						}
+					}
+				}
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	protected void loadTiles() {
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-				tileCheck: for (Tile t : Tile.tiles) {
-					if (t != null
-							&& t.getLevelColour() == tileColours[x + y * width]) {
-						if (t == Tile.GRASS) {
-							t = Tile.GRASS();
-						} else if (t == Tile.WASTELAND_GROUND1) {
-							t = Tile.CONCRETE();
-						}
-						this.tiles[x + y * width] = t.getId();
-						break tileCheck;
-					}
-				}
-			}
-		}
-	}
-
+	/**
+	 * Generates a random level
+	 */
 	protected void generateLevel() {
+
+		// fill it with grass and stone
+		// usually overridden in another class
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
 				if (x * y % 10 < 10) {
@@ -174,35 +212,34 @@ public abstract class Level implements Serializable {
 
 	}
 
-	protected void saveLevelToFile() {
-		try {
-			ImageIO.write(image, "png",
-					new File(Level.class.getResource(this.imagePath).getFile()));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
+	/**
+	 * Changes a tile on the map
+	 * 
+	 * @param x
+	 *            the x coord of the tile
+	 * @param y
+	 *            the y coord of the tile
+	 * @param newTile
+	 *            the tile to replace it with
+	 */
 	public void alterTile(int x, int y, Tile newTile) {
 		this.tiles[x + y * width] = newTile.getId();
-		if (image != null) {
-			image.setRGB(x, y, newTile.getLevelColour());
-			tileColours[x + y * width] = newTile.getId();
-		}
-
 	}
 
+	/**
+	 * Removes dead mobs from the level
+	 */
 	public void clear() {
-		for (Mob m : killList) {
-			this.remEntity(m);
-			killList.remove(m);
+		for (Mob m : mobs) {
+			if (m.isDead())
+				remove(m);
 		}
 	}
 
 	public void tick() {
-		
+
 		for (Entity e : this.getEntities()) {
-			if (!e.getBounds().intersects(screenRectangle)) {
+			if (!e.getBounds().intersects(renderRange)) {
 				continue;
 			}
 			if (e instanceof Mob) {
@@ -242,11 +279,10 @@ public abstract class Level implements Serializable {
 
 	public void renderEntities(Screen screen) {
 
-		screenRectangle.setLocation(Game.player.getX() - 250,
-				Game.player.getY() - 250);
+		renderRange.setLocation(Game.player.getX() - 250, Game.player.getY() - 250);
 
 		for (Entity entity : this.getEntities()) {
-			if (!entity.getBounds().intersects(screenRectangle)) {
+			if (!entity.getBounds().intersects(renderRange)) {
 				continue;
 			}
 			if (entity instanceof Projectile) {
@@ -265,7 +301,7 @@ public abstract class Level implements Serializable {
 			}
 		}
 		for (Entity e : this.getEntities()) {
-			if (!e.getBounds().intersects(screenRectangle)) {
+			if (!e.getBounds().intersects(renderRange)) {
 				continue;
 			}
 			if (!(e instanceof Mob || e instanceof HealthBar || e instanceof Projectile)) {
@@ -273,7 +309,7 @@ public abstract class Level implements Serializable {
 			}
 		}
 		for (Entity entity : this.getEntities()) {
-			if (!entity.getBounds().intersects(screenRectangle)) {
+			if (!entity.getBounds().intersects(renderRange)) {
 				continue;
 			}
 			if (entity instanceof Projectile) {
@@ -293,8 +329,7 @@ public abstract class Level implements Serializable {
 		}
 	}
 
-	public void renderFont(String msg, Screen screen, int x, int y,
-			int[] color, int scale) {
+	public void renderFont(String msg, Screen screen, int x, int y, int[] color, int scale) {
 		JJFont.render(msg, screen, x, y, color, scale);
 	}
 
@@ -319,7 +354,7 @@ public abstract class Level implements Serializable {
 			if (entity instanceof Player) {
 				this.players.add((Player) entity);
 			}
-		} 
+		}
 	}
 
 	public void addEntity(Entity entity, int index) {
@@ -332,7 +367,7 @@ public abstract class Level implements Serializable {
 			if (entity instanceof Player) {
 				this.players.add(index, (Player) entity);
 			}
-		} 
+		}
 
 	}
 
@@ -343,7 +378,7 @@ public abstract class Level implements Serializable {
 			if (entity instanceof Player) {
 				this.players.remove((Player) entity);
 			}
-		} 
+		}
 	}
 
 	public void reset() {
@@ -370,7 +405,7 @@ public abstract class Level implements Serializable {
 	public Player getPlayer() {
 		if (players.size() > 0)
 			return players.get(0);
-		else 
+		else
 			return null;
 	}
 
@@ -393,9 +428,9 @@ public abstract class Level implements Serializable {
 			initMobLocations();
 		}
 	}
-	
+
 	public void initMobLocations() {
-		for (Mob m: this.getMobs()) {
+		for (Mob m : this.getMobs()) {
 			m.updateBounds();
 		}
 	}
@@ -403,11 +438,11 @@ public abstract class Level implements Serializable {
 	public String toString() {
 		return name + "\n Mobs: " + this.getMobs();
 	}
-	
+
 	public Screen getScreen() {
 		return screen;
 	}
-	
+
 	public void setSpawnPoint(int x, int y) {
 		spawnPoint = new Point(x, y);
 	}
