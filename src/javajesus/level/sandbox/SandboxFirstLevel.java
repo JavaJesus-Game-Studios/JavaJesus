a296c22c70dd229f31cf45bc17ff9785049a5e9b
Andrew Leamy <drewn7512@gmail.com>
2017-07-17 17:07:18 -0700

    Test Level

---
+++ src/javajesus/level/sandbox/SandboxFirstLevel.java
@@ -0,0 +1,90 @@
+package javajesus.level.sandbox;
+
+import java.awt.Point;
+
+import javajesus.entities.Entity;
+import javajesus.entities.Spawner;
+import javajesus.entities.npcs.NPC;
+import javajesus.entities.solid.furniture.Chest;
+import javajesus.entities.transporters.MapTransporter;
+import javajesus.level.Level;
+
+/*
+ * Have comments here about which level it is
+ * 
+ * 1. extend level
+ * 2. add constructor w/ spawn
+ * 3. add unimplemented methods
+ */
+public class SandboxFirstLevel extends Level
+{
+
+    /**
+     * Initializes the First Level
+     * 
+     * @param imagePath - the level file path
+     * @param loadNow - whether or not to load now // always false for sandbox
+     * @param name - Name of this level
+     * @param spawn - where the player first spawns
+     */
+    public SandboxFirstLevel()
+    {
+        super("/Levels/Sandbox/first level.png", false, "First map", new Point(100, 100));
+    }
+
+    /**
+     * Where NPCs are loaded
+     */
+    @Override
+    protected NPC[] getNPCPlacement()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * Where spawners are placed
+     */
+    @Override
+    protected Spawner[] getSpawnerPlacement()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * Where chests are placed
+     */
+    @Override
+    protected Chest[] getChestPlacement()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * Where Transporter Tiles are placed
+     * These transport you from one level to another
+     * probably wont use these unless you plan on going inside
+     * buildings
+     */
+    @Override
+    protected MapTransporter[] getMapTransporterPlacement()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * Anything else
+     * This includes trees, forests, something that
+     * wasnt mentioned above
+     */
+    @Override
+    protected Entity[] getOtherPlacement()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
