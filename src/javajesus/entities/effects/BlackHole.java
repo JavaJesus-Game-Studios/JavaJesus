dfb26ff2bd278f2b53fec628bdf6bf2f8024f10a
Derek <derek.jow@gmail.com>
2017-07-20 15:08:36 -0700

    Merge Conflict Fixes

--- src/javajesus/entities/effects/BlackHole.java
+++ src/javajesus/entities/effects/BlackHole.java
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-
-
-=======
->>>>>>> origin/master
 package javajesus.entities.effects;
 
 import java.awt.geom.Ellipse2D;
@@ -19,19 +14,6 @@ import javajesus.level.Level;
  */
 public class BlackHole extends Entity {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-	// serialization
-	private static final long serialVersionUID = 2827325538515820858L;
-
-=======
->>>>>>> origin/master
-=======
-	// serialization
-	private static final long serialVersionUID = 2827325538515820858L;
-
-
->>>>>>> c189370753afe89e176e2b9840abf29c5409c19d
 	// xTile offset
 	private int xOffset;
 
@@ -54,15 +36,9 @@ public class BlackHole extends Entity {
 	private static final int DPT = 1;
 
 	// spritesheet of black hole
-<<<<<<< HEAD
-	private static final SpriteSheet sheet = SpriteSheet.explosionSmall;
-
-	 //color of the black hole
-=======
 	private static final SpriteSheet sheet = SpriteSheet.explosionLarge;
 
 	// color of the black hole
->>>>>>> origin/master
 	private static final int[] color = { 0xFF000000, 0xFF000000, 0xFF000000 };
 
 	// box size on spritesheet
@@ -77,15 +53,8 @@ public class BlackHole extends Entity {
 	 */
 	public BlackHole(Level level, int x, int y) {
 		super(level, x, y);
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-		 //set collision bounds
-=======
-=======
->>>>>>> c189370753afe89e176e2b9840abf29c5409c19d
 		// set collision bounds
->>>>>>> origin/master
 		setBounds(getX() - (MODIFIER * 2), getY() - (MODIFIER * 2), MODIFIER * 4, MODIFIER * 4);
 		radius = new Ellipse2D.Double(x - SIZE / 2, y - SIZE / 2, SIZE, SIZE);
 
@@ -95,40 +64,20 @@ public class BlackHole extends Entity {
 	 * Updates the black hole
 	 */
 	public void tick() {
-<<<<<<< HEAD
-
-<<<<<<< HEAD
+		
 		 //increment animation
-=======
-		// increment animation
->>>>>>> origin/master
-=======
-		 //increment animation
->>>>>>> c189370753afe89e176e2b9840abf29c5409c19d
 		if (++tickCount % ANIMATION_LENGTH == 0) {
 			xOffset += 4;
 		}
 
 		// animation is over
 		if (xOffset > (14 * 4)) {
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-			 //remove the blackhole
-=======
-=======
->>>>>>> c189370753afe89e176e2b9840abf29c5409c19d
 			// remove the blackhole
->>>>>>> origin/master
 			getLevel().remove(this);
 
 		}
 
-<<<<<<< HEAD
 		 //randomly create an explosion
-=======
-		// randomly create an explosion
->>>>>>> origin/master
 		if (random.nextInt(10) == 0) {
 			getLevel().add(
 			        new Explosion(getLevel(), random.nextInt(300) - 150 + getX(), random.nextInt(300) - 150 + getY()));
@@ -155,15 +104,8 @@ public class BlackHole extends Entity {
 					dy++;
 				}
 			}
-<<<<<<< HEAD
-
-<<<<<<< HEAD
+			
 			 //move the mob
-=======
-=======
->>>>>>> c189370753afe89e176e2b9840abf29c5409c19d
-			// move the mob
->>>>>>> origin/master
 			if (dx != 0 || dy != 0) {
 				mob.move(dx, dy);
 			}
@@ -176,17 +118,10 @@ public class BlackHole extends Entity {
 	 */
 	public void render(Screen screen) {
 
-<<<<<<< HEAD
-		 //top to bottom
-		for (int i = 0; i < 4; i++) {
-
-			 //left to right
-=======
 		// top to bottom
 		for (int i = 0; i < 4; i++) {
 
 			// left to right
->>>>>>> origin/master
 			for (int j = 0; j < 4; j++) {
 				screen.render(getX() - (MODIFIER * 2) + (j * MODIFIER), getY() - (MODIFIER * 2) + (i * MODIFIER),
 				        xOffset + j, i, sheet, false, color);
@@ -194,8 +129,6 @@ public class BlackHole extends Entity {
 		}
 
 	}
-<<<<<<< HEAD
-=======
 
 	/**
 	 * Black Holes won't be saved
@@ -204,6 +137,5 @@ public class BlackHole extends Entity {
 	public byte getId() {
 		return -1;
 	}
->>>>>>> origin/master
 
 }
