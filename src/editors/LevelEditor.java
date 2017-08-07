a2cfbed1f1b7e5621774a804039e613101838c56
Derek <derek.jow@gmail.com>
2017-08-06 19:02:36 -0700

    bug fixes
    all byte ids over 127 will need to be bit wise ANDED with a short/int to get the positive value

--- src/editors/LevelEditor.java
+++ src/editors/LevelEditor.java
@@ -1296,8 +1296,8 @@ public class LevelEditor extends JPanel implements MouseListener, ActionListener
 			entityList[Entity.LARGE_TREE] = new EntityGUI(new LargeTree(null, 0, 0), PANEL_SIZE, PANEL_SIZE, 4, 7);
 			entityList[Entity.LARGE_TREE_AUTUMN] = new EntityGUI(new LargeTreeAutumn(null, 0, 0), PANEL_SIZE, PANEL_SIZE, 4, 7);
 			entityList[Entity.LARGE_TREE_WINTER] = new EntityGUI(new LargeTreeWinter(null, 0, 0), PANEL_SIZE, PANEL_SIZE, 4, 7);
-			entityList[Entity.WHITE_OAK] = new EntityGUI(new WhiteOak(null, 0, 0), PANEL_SIZE, PANEL_SIZE, 5, 7);
-			entityList[Entity.WHITE_OAK_SMALL] = new EntityGUI(new WhiteOakSmall(null, 0, 0), PANEL_SIZE, PANEL_SIZE, 4, 6);
+			entityList[Entity.WHITE_OAK & 0x00FF] = new EntityGUI(new WhiteOak(null, 0, 0), PANEL_SIZE, PANEL_SIZE, 5, 7);
+			entityList[Entity.WHITE_OAK_SMALL & 0x00FF] = new EntityGUI(new WhiteOakSmall(null, 0, 0), PANEL_SIZE, PANEL_SIZE, 4, 6);
 			
 		} catch (IOException e) {
 			e.printStackTrace();
