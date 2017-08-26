a0b9750e9bc523761be37e4eeb194fed3c98ca01
Shtevay <stephennway@gmail.com>
2017-08-25 17:56:45 -0700

    Quest Updates

--- src/javajesus/quest/original/AlphaTurningTheTide.java
+++ src/javajesus/quest/original/AlphaTurningTheTide.java
@@ -1,9 +1,31 @@
 package javajesus.quest.original;
 
+import java.util.Iterator;
+import java.util.List;
+
+import javajesus.entities.Mob;
+import javajesus.entities.monsters.Demon;
+import javajesus.entities.monsters.EvilOrangutan;
 import javajesus.entities.npcs.NPC;
+import javajesus.entities.npcs.characters.Jesus;
+import javajesus.level.sandbox.SandboxOriginalLevel;
 import javajesus.quest.Quest;
 
 public class AlphaTurningTheTide extends Quest{
+	
+	//The two lists of mobs to iterate through
+	List<Mob> list1=giver.getLevel().getMobs();
+	List<Mob> list2=SandboxOriginalLevel.alpha.getMobs();
+	
+	//Mob instance
+	Mob m;
+	
+	//Iterates that will iterate through the lists
+	Iterator<Mob> it1 = list1.iterator();
+	Iterator<Mob> it2 = list2.iterator();
+	
+	private static final int ISDEAD = 0;
+
 
 	public AlphaTurningTheTide(NPC giver){
 		super(giver, "/WORLD_DATA/QUEST_DATA/ALPHA_TURNING_THE_TIDE.json", 1);
@@ -11,8 +33,19 @@ public class AlphaTurningTheTide extends Quest{
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
-		
+		// if a demon is not found, assume true
+				boolean killed = true;
+				
+				//If either a Jesus or an EvilOrangutan instance is killed, the quest updates
+				while(it1.hasNext() && it2.hasNext()) {
+					if (m instanceof Jesus && !m.isDead() || m instanceof EvilOrangutan && !m.isDead()) {
+						killed = false;
+						break;
+					}
+				}
+			
+				// update the objective
+				objectives[ISDEAD] = killed;
 	}
 	
 }
